58638ac005503808e779d68c8c14a36f22eb75ab
djbvvo <101988672+djbvvo@users.noreply.github.com>
2026-09-24 09:50:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -94,4 +94,4 @@ def actualizar_grafico(variable_seleccionada):
 
 # 6. Ejecutar la aplicación (Configurado para Binder)
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=False)
+    app.run(host='0.0.0.0', port=8050, debug=False)
